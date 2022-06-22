fcee0faaa6b9db767103755f6f2fc6f569e31fa6
Adam Patrick <98496625+adam-patrick@users.noreply.github.com>
2022-06-22 14:42:36 -0500

    Add files via upload

---
+++ Section 6/Inheritance/src/Fish.java
@@ -0,0 +1,29 @@
+public class Fish extends Animal{
+
+    private int gills;
+    private int eyes;
+    private int fins;
+
+    public Fish(String name, int brain, int body, int size, int weight, int gills, int eyes, int fins) {
+        super(name, 1, 1, size, weight);
+        this.gills = gills;
+        this.eyes = eyes;
+        this.fins = fins;
+    }
+
+    public void rest(){
+
+    }
+    private void moveMuscles(){
+
+    }
+
+    private void moveBackFin(){
+
+    }
+    public void swim(int speed){
+        moveMuscles();
+        moveBackFin();
+        super.move(speed);
+    }
+}
